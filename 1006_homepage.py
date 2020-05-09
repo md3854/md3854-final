243a5d84e33afbaca0337628aded8cdcb5f64382
Max Dickman <md3854@columbia.edu>
2020-05-09 17:42:55 -0400

    Finished main page, added assignments html document.

---
+++ 1006_homepage.py
@@ -0,0 +1,30 @@
+######################################
+#
+# Author: md3854
+#
+# Engi1006 final
+#
+######################################
+
+#import statements
+from flask import Flask, render_template
+
+#Flask app variable
+app = Flask(__name__)
+
+#static route
+@app.route("/")
+def welcome():
+    return render_template("1006_homepage.html")
+
+@app.route("/projects")
+def projects():
+    return render_template("1006.html")
+
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+#start the server
+if __name__ == "__main__":
+    app.run()
